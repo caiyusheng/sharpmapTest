dfe416acbf79caa0836a0887a7ce64bb34ec9f70
xiangyucai <xiangyucai@foxmail.com>
2018-03-19 09:09:28 +0800

    修改命名空间

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ShapeFile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mapBox1
+            // 
+            this.mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.Pan;
+            this.mapBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.mapBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mapBox1.CustomTool = null;
+            this.mapBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mapBox1.FineZoomFactor = 10D;
+            this.mapBox1.Location = new System.Drawing.Point(0, 0);
+            this.mapBox1.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.mapBox1.Name = "mapBox1";
+            this.mapBox1.PreviewMode = SharpMap.Forms.MapBox.PreviewModes.Fast;
+            this.mapBox1.QueryGrowFactor = 5F;
+            this.mapBox1.QueryLayerIndex = 0;
+            this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.ShowProgressUpdate = true;
+            this.mapBox1.Size = new System.Drawing.Size(800, 395);
+            this.mapBox1.TabIndex = 4;
+            this.mapBox1.Text = "mapBox1";
+            this.mapBox1.WheelZoomMagnitude = -2D;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 459);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "MSsql";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.mapBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private SharpMap.Forms.MapBox mapBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using SharpMap;
+using SharpMap.Layers;
+using SharpMap.Data.Providers;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        // we start with the connection string
+        static string connstr = "Data Source=192.168.2.60;Initial Catalog=gisdb;Persist Security Info=True;User ID=sa;Password=cai123";
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“gisdbDataSet.PT_TOWN”中。您可以根据需要移动或删除它。
+           // this.pT_TOWNTableAdapter.Fill(this.gisdbDataSet.PT_TOWN);
+
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            VectorLayer layCountries = new VectorLayer("Countries");
+            layCountries.DataSource = new ShapeFile("GeoData/World/countries.shp", true);
+            layCountries.Style.Fill = new SolidBrush(Color.GreenYellow);
+            layCountries.Style.Outline = Pens.Black;
+            layCountries.Style.EnableOutline = true;
+            layCountries.SRID = 4326;
+
+
+            this.mapBox1.Map.Layers.Add(layCountries);
+
+            this.mapBox1.Map.ZoomToExtents();
+            this.mapBox1.Refresh();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.mapBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VectorLayer layCountries = new VectorLayer("Mssql");
+            //SqlServer2008 MSSQLDP = new SharpMap.Data.Providers.SqlServer2008(connstr, "PT_TOWN", "ID", SqlServerSpatialObjectType.Geography);
+            //MSSQLDP.Table = "gisdb.dbo.PT_TOWN";
+            //MSSQLDP.TableSchema = String.Empty;
+             layCountries.DataSource = new SharpMap.Data.Providers.SqlServer2008(connstr, "gisdb.dbo.PT_TOWN", "geom","ID",SqlServerSpatialObjectType.Geography); 
+            //  (connstr, "PT_TOWN", "geom", "ID");
+           // layCountries.DataSource = MSSQLDP;
+
+
+            layCountries.Style.Fill = new SolidBrush(Color.GreenYellow);
+            layCountries.Style.Outline = Pens.Black;
+            layCountries.Style.EnableOutline = true;
+            //layCountries.SRID = 4326;
+            
+
+            this.mapBox1.Map.Layers.Add(layCountries);
+            this.mapBox1.Map.ZoomToExtents();
+            this.mapBox1.Refresh();
+
+
+
+        }
+    }
+}
